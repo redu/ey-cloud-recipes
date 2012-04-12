544e932bde6376586fea1cb7516aa68140623273
fltiago <fltiago@gmail.com>
2012-04-12 14:03:24 -0300

    Adding mongodb_initializer to recipes

---
+++ cookbooks/mongodb_initializer/recipes/default.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook Name:: mongodb_initializer
+# Recipe:: default
+#
+
+require_recipe "mongodb_initializer::intall"
+require_recipe "mongodb_initializer::start"
---
+++ cookbooks/mongodb_initializer/recipes/install.rb
@@ -0,0 +1,13 @@
+# TO-DO: replace with portage version. Check for 32bit on i686 systems (for arbiter only installs)
+file_to_fetch = "http://fastdl.mongodb.org/linux/mongodb-linux-i686-2.0.4.tgz"
+
+execute "fetch #{file_to_fetch}" do
+  cwd "/tmp"
+  command "wget #{file_to_fetch}"
+  not_if { FileTest.exists?("/tmp/mongodb-linux-i686-2.0.4.tgz") }
+end
+
+execute "untar /tmp/mongodb-linux-i686-2.0.4.tgz" do
+  command "cd /tmp; tar zxf mongodb-linux-i686-2.0.4.tgz -C /opt"
+  not_if { FileTest.directory?("/opt/mongodb-linux-i686-2.0.4") }
+end
