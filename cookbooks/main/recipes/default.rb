89e821e5ed56a6e79f9bf4a8904697d277c8af7d
fltiago <fltiago@gmail.com>
2012-04-12 16:41:21 -0300

    Adding mongodb_initializer in main

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -36,6 +36,7 @@
 #uncomment to run the exim::auth recipe
 #require_recipe "exim::auth"
 #require_recipe "mongodb"
+require_recipe "mongodb_initializer"
 
 #uncomment to run the resque recipe
 # require_recipe "resque"
@@ -98,7 +99,7 @@
   # postgresql9_isn "dbname"
   # postgresql9_lo "dbname"
   # postgresql9_ltree "dbname"
-  # postgresql9_pg_trgm "dbname"  
+  # postgresql9_pg_trgm "dbname"
   # postgresql9_pgcrypto "dbname"
   # postgresql9_pgrowlocks "dbname"
   # postgresql9_postgis "dbname"
@@ -108,15 +109,15 @@
   # postgresql9_test_parser "dbname"
   # postgresql9_unaccent "dbname"
   # postgresql9_uuid_ossp "dbname"
-  
-  
+
+
   # 9.1 Extensions
-  # postgresql9_file_fdw "dbname" 
+  # postgresql9_file_fdw "dbname"
   # postgresql9_xml2 "dbname"
-  
+
   #Admin-Level Contribs
   # postgresql9_pg_buffercache "postgres"
   # postgresql9_pg_freespacemap "postgres"
   # postgresql9_pg_stat_statements "todo" - Not done
-  
+
 # end
