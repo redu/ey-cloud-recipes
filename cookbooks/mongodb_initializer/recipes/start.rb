cc4178720baeade3d4b92a610522f9e8f0549af7
fltiago <fltiago@gmail.com>
2012-04-12 22:50:04 -0300

    Modifyng start recipe, to correct path

--- cookbooks/mongodb_initializer/recipes/start.rb
+++ cookbooks/mongodb_initializer/recipes/start.rb
@@ -1,6 +1,6 @@
 execute "start mongodb" do
   Chef::Log.info "Executing Mondodb"
-  command "/tmp/mongodb-linux-i686-2.0.4/bin/mongod"
+  command "/opt/mongodb-linux-i686-2.0.4/bin/mongod"
   Chef::Log.info "Mongodb executed"
   action :run
 end
