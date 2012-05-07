3e322d6567081925c62b22aad7ee32f4983e7fdf
fltiago <fltiago@gmail.com>
2012-05-07 20:48:04 -0300

    Removing action run.

--- cookbooks/mongodb_initializer/recipes/install.rb
+++ cookbooks/mongodb_initializer/recipes/install.rb
@@ -22,5 +22,4 @@ execute "start mongodb" do
   command "sudo /opt/mongodb-linux-i686-2.0.4/bin/mongod --repair"
   command "sudo /opt/mongodb-linux-i686-2.0.4/bin/mongod --fork --logpath /var/log/mongodb.log --logappend"
   Chef::Log.info "Mongodb executed"
-  action :run
 end
