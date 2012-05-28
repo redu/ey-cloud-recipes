068a130a759f50df24d13d517e63e537c5a319b7
fltiago <fltiago@gmail.com>
2012-05-28 16:08:45 -0300

    Adding logs and actions to each execute command.

--- cookbooks/mongodb_initializer/recipes/install.rb
+++ cookbooks/mongodb_initializer/recipes/install.rb
@@ -2,18 +2,24 @@
 file_to_fetch = "http://fastdl.mongodb.org/linux/mongodb-linux-i686-2.0.4.tgz"
 
 execute "fetch #{file_to_fetch}" do
+  Chef::Log.info "Downloading MongoDB file"
   cwd "/tmp"
   command "wget #{file_to_fetch}"
+  action :install
   not_if { FileTest.exists?("/tmp/mongodb-linux-i686-2.0.4.tgz") }
 end
 
 execute "untar /tmp/mongodb-linux-i686-2.0.4.tgz" do
+  Chef::Log.info "Copying mongodb file"
   command "cd /tmp; tar zxf mongodb-linux-i686-2.0.4.tgz -C /opt"
+  action :install
   not_if { FileTest.directory?("/opt/mongodb-linux-i686-2.0.4") }
 end
 
 execute "creating a symbolik link" do
+  Chef::Log.info "Creating a symbolic link to mongodump"
   command "ln -s /opt/mongodb-linux-i686-2.0.4/bin/mongodump /usr/bin/mongodump"
+  action :install
   not_if { FileTest.exists?("/usr/bin/mongodump") }
 end
 
@@ -21,5 +27,6 @@ execute "start mongodb" do
   Chef::Log.info "Executing Mongodb"
   command "sudo /opt/mongodb-linux-i686-2.0.4/bin/mongod --repair"
   command "sudo /opt/mongodb-linux-i686-2.0.4/bin/mongod --fork --logpath /var/log/mongodb.log --logappend"
+  action :start
   Chef::Log.info "Mongodb executed"
 end
